60fe0d8cf9370872c9c9fd8e3cc23a6a886d2b05
Vishwas Srivastava <vishwassrivastava1991@gmail.com>
2022-04-14 01:32:15 +0530

    Initial commit

---
+++ src/Heading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Heading() {
+  return <h1>shopping items to buy </h1>;
+}
+
+export default Heading;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading";
+
+ReactDOM.render(
+  <div>
+    <Heading></Heading>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
